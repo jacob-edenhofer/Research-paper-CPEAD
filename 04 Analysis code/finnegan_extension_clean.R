bb6a6a1f1137ae1e913f510c664012fb960f20e9
Jacob Edenhofer <jacob.edenhofer98@gmail.com>
2024-05-25 15:07:13 +0100

    updated finnegan analysis

--- 04 Analysis code/finnegan_extension_clean.R
+++ 04 Analysis code/finnegan_extension_clean.R
@@ -21,25 +21,37 @@ finnegan_merged <- readRDS(data_path)
 finnegan_merged <- finnegan_merged %>% zap_labels()
 
 ## variable dictionary 
-var_dict_finnegan <- c("lambda_mean_wghtd" = "Mean of the weighted lambda values",
-                       "lambda_mean_wghtd_con" = "Mean of the weighted lambda values for the consumption sector",
-                       "lambda_mean_wghtd_prod" = "Mean of the weighted lambda values for the production sector",
-                       "lambda_mean_wghtd_comp" = "Mean of the weighted lambda values for the compensation sector",
+var_dict_finnegan <- c("lambda_mean_wghtd" = "Overall stringency",
+                       "lambda_mean_wghtd_con" = "Costs for consumers",
+                       "lambda_mean_wghtd_prod" = "Costs for producers",
+                       "lambda_mean_wghtd_comp" = "Consumer vs. producer costs",
                        "openc.x" = "Openness to trade",
-                       "ind_valueadd" = "Industrial value added",
+                       "ind_valueadd" = "Industry value added",
+                       "fossfuel_prodpercap" = "Fossil fuel production per capita",
+                       "realgdpgr.x" = "Real GDP growth",
+                       "unemp" = "Unemployment rate",
                        "elect_comp" = "Electoral competitiveness",
-                       "ri" = "Resource intensity",
-                       "corp_all" = "All corporatism",
-                       "corp_allsm" = "All corporatism (smoothed)",
-                       "corp_core" = "Core corporatism",
-                       "corp_cor_esm" = "Corporatism (ESM)",
-                       "corpo_f_cor_esm" = "Corporatism (ESM) with fixed effects",
-                       "bc" = "Bargaining coordination",
-                       "tc" = "Trade union coordination",
+                       "pr_ingov_mean_annual" = "Green party CIP",
+                       "happy_with_env_preserv" = "Happiness with environmental preservation",
+                       "unhappy_with_env_preserv" = "Unhappiness with environmental preservation",
+                       "ri" = "Concertation",
+                       "dis_gall.y" = "Gallgher's disproportionality index",
+                       "elderly.x" = "Share of >65",
+                       "corp_all" = "Corporatism (all)",
+                       "corp_allsm" = "Corporatism (all, smoothed)",
+                       "corp_core" = "Corporatism (core)",
+                       "corp_cor_esm" = "Corporatism (core, smoothed)",
+                       "corpo_f_cor_esm" = "Corporatism (ESM)",
+                       "bc" = "Bipartite concertation",
+                       "tc" = "Tripartite concertation",
                        "ud" = "Union density",
-                       "nec_fs" = "Non-employment costs (full sample)",
-                       "nuc_fs" = "Non-union coverage (full sample)",
-                       "adj_cov_hist" = "Adjusted coverage (historical)")
+                       "nec_fs" = "number of employer confederations",
+                       "nuc_fs" = "number of union confederations",
+                       "ja20f_v2" = "Green vs. growth government preferences",
+                       "carbon_inten1" = "Carbon intensity of energy supply",
+                       "adj_cov_hist" = "Adjusted coverage (historical)",
+                       "countryid" = "Country", 
+                       "year" = "Year")
 
 
 ################################
@@ -61,10 +73,11 @@ run_model <- function(formula, data, model_name) {
 # Analysis: Extending Finnegan's Analysis
 ################################
 
+
 # Define variable names and models
-variables <- c("openc.x", "ind_valueadd", "elect_comp")
+variables <- c("openc.x", "ind_valueadd", "elect_comp", "carbon_inten1", "pr_ingov_mean_annual", "happy_with_env_preserv", "unhappy_with_env_preserv")
 dvs_v <- c("lambda_mean_wghtd", "lambda_mean_wghtd_con", "lambda_mean_wghtd_prod", "lambda_mean_wghtd_comp")
-corporatism <- c("ri", "corp_all", "corp_allsm", "corp_core", "corp_cor_esm", "corpo_f_cor_esm", "bc", "tc", "ud", "nec_fs", "nuc_fs", "adj_cov_hist")
+corporatism <- c("ri", "corp_all", "corp_allsm", "corp_core", "corp_cor_esm", "bc", "tc")
 
 # Factorise some of the corporatism variables
 finnegan_merged <- finnegan_merged %>%
@@ -78,22 +91,32 @@ plots_list <- list()
 for (var1 in corporatism) {
   for (dv in dvs_v) {
     for (var2 in variables) {
-      formula <- as.formula(paste0(dv, "~", var1, "*", var2, " + fossfuel_prodpercap + realgdpgr.x + unemp + ja20f_v2 | countryid + year"))
+      formula <- as.formula(paste0(dv, "~", var1, "*", var2, " + fossfuel_prodpercap + realgdpgr.x + unemp + elderly.x + ja20f_v2 | countryid + year"))
       model_name <- paste(dv, var1, var2, sep = "__")
       models_list[[model_name]] <- run_model(formula, data = finnegan_merged, model_name)
       
       ## save regression table
+      table_path <- here("06 Figures and tables", "Tables", "Finnegan", paste0(model_name, ".tex"))
+      # formula_table <- as.formula(paste0(dv, "~", var1, "*", var2, " + csw0(fossfuel_prodpercap, realgdpgr.x + unemp, ja20f_v2) | countryid + year"))
+      formula_table <- as.formula(paste0(dv, "~", var1, " + csw0( ", var2, ",", var1, ":", var2, ", fossfuel_prodpercap, realgdpgr.x, unemp, elderly.x, ja20f_v2) | countryid + year"))
+      model_table <- feols(fml = formula_table, data = finnegan_merged)
+      etable(model_table, 
+             tex = T, 
+             dict = var_dict_finnegan,
+             replace = T, 
+             title = paste("Examining", var_dict_finnegan[dv]),
+             file = table_path)
       
       ## me plot
       plots_list[[model_name]] <- plot_slopes(models_list[[model_name]], rug = T,
                                              variables = var1,
                                              by = var2) +
         geom_hline(yintercept = 0, linetype = "dashed") +
-        labs(title = paste("Marginal Effect of", var1, "on", dv, "by", var2),
-             x = var2) +
+        labs(title = paste("Marginal effect of", var_dict_finnegan[var1], "\non", var_dict_finnegan[dv], "by", var_dict_finnegan[var2]),
+             x = var_dict_finnegan[var2]) +
         theme_bw()
       print(plots_list[[model_name]])
-      ggsave(paste0(here(), "/06 Figures and tables/Figures/ME exploratory/", model_name, ".png"), 
+      ggsave(paste0(here(), "/06 Figures and tables/Figures/ME Finnegan/", model_name, ".png"), 
              width = 9, height = 6, unit = "in", dpi = 300)
     }
   }
